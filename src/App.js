24edf865726a3e15bc92c6ffda52cf281b04fc0f
king0203 <darshanarkhade@gmail.com>
2023-07-30 01:22:21 +0530

    done

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from "react";
 import { Routes, Route, Link } from "react-router-dom";
 import { Layout, Typography, Space } from "antd";
 import {
-  Exchanges,
   Homepage,
   News,
   Cryptocurrencies,
@@ -22,7 +21,6 @@ const App = () => {
           <div className="routes">
             <Routes>
               <Route path="/" element={<Homepage />} />
-              <Route path="/exchanges" element={<Exchanges />} />
               <Route path="/cryptocurrencies" element={<Cryptocurrencies />} />
               <Route path="/crypto/:coinId" element={<CryptoDetails />} />
               <Route path="/news" element={<News />} />
